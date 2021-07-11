c7d2b6c9c8d9cc27efe1c0ed51c66e05d74d6884
poornima choudhary <adarsh16007@iiitd.ac.in>
2021-07-12 05:12:53 +0530

    backend

---
+++ contest/backend/ide/file.py
@@ -0,0 +1,8 @@
+for i in range(int(input())):
+    t = int(input())
+    print(t)
+
+#cat input.txt | python3 file.py > output.txt
+#cat input.txt | node file.js > output.txt
+#jcar() { javac $1.java && java $1 && rm $1.class; }
+#cat input.txt | jcar file
